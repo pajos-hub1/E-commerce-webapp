c9c70be0a32b141e1a6348a34065ca246243ee9f
pajos-hub1 <55576435+pajos-hub1@users.noreply.github.com>
2021-09-27 17:10:32 -0700

    Add files via upload

---
+++ realtors/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_mvp', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_mvp',)
+    search_fields = ('name',)
+    list_per_page = 25
+    list_filter = ('name',)
+
+
+admin.site.register(Realtor, RealtorAdmin)
